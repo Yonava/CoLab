4276febaf30bdafd2a58eacc9d8c9d20215cb6c1
yonava <yona@vossandreae.com>
2023-11-11 03:38:04 -0500

    formatting panel

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -5,7 +5,7 @@
       style="padding: 15px; display: flex; flex-direction: row; align-items: center; gap: 10px;"
     >
       <h1 style="font-size: 2.7em">
-        Project List
+        Reports
       </h1>
       <v-spacer></v-spacer>
       <v-btn icon>
@@ -21,11 +21,13 @@
     </div>
 
     <!-- list box -->
-    <div>
+    <div style="display: flex; justify-content: center; align-items: center;">
+    <div style="width: 95%; height: 90vh; padding: 15px; background-color: rgba(255, 255, 255, 0.2); border-radius: 10px; overflow: auto">
       <!-- item box -->
-      <div>
-
+      <div v-for="report in reports" :key="report.name">
+        {{ reports  }}
       </div>
+    </div>
     </div>
 
   </div>
@@ -36,5 +38,21 @@
 // import { useSheetManager } from '../../store/useSheetManager'
 // import { storeToRefs } from 'pinia'
 
-
+const reports = [
+  {
+    name: 'Report 1',
+    client: 'Client 1',
+    date: '2021-09-01',
+    status: 'In Progress',
+    desc: 'Lorem ipsum dolor sit amet, consectetur'
+  },
+  // generate 5 more reports
+  ...Array.from({ length: 5 }, (_, i) => ({
+    name: `Report ${i + 2}`,
+    client: `Client ${i + 2}`,
+    date: '2021-09-01',
+    status: 'In Progress',
+    desc: 'Lorem ipsum dolor sit amet, consectetur'
+  }))
+]
 </script>
