1d5324097fd9dde7385da987eba84cefb7088fca
Thomas Eykamp <embergameslp@gmail.com>
2023-11-11 22:06:02 -0500

    add emailing to client page

--- client/src/components/ReportDetail.vue
+++ client/src/components/ReportDetail.vue
@@ -1,63 +1,33 @@
 <template>
-  <div style="overflow: auto">
+  <div>
     <DetailFrame :item="report">
       <template #main>
 
-        <DetailHeader
-          :item="report"
-          placeholder="Course Code"
-        ></DetailHeader>
+        <DetailHeader :item="report" placeholder="Course Code"></DetailHeader>
 
-        <DetailInput
-          :item="report"
-          prop="managerEmail"
-          label="Manager Email"
-          icon="email"
-        />
+        <DetailInput @submit.prevent="sendEmail" :item="report" prop="managerEmail" label="Manager Email" icon="email" />
 
-        <DetailInput
-          :item="report"
-          prop="client"
-          label="Client"
-          icon="account"
-        />
+        <DetailInput :item="report" prop="client" label="Client" icon="account" />
 
         <InputCoupler>
 
-          <DetailInput
-            :item="report"
-            prop="date"
-            icon="calendar-month"
-            label="Date"
-          />
+          <DetailInput :item="report" prop="date" icon="calendar-month" label="Date" />
 
-          <DetailInput
-            :item="report"
-            prop="status"
-            icon="check-circle"
-            label="Status"
-          />
+          <DetailInput :item="report" prop="status" icon="check-circle" label="Status" />
 
         </InputCoupler>
 
       </template>
       <template #buttons>
-        <v-btn
-          @click="sendEmail"
-          size="large"
-          color="green"
-        >
-          <v-icon
-            class="mr-4"
-            size="x-large"
-          >
+        <v-btn @click="sendEmail" size="large" color="green" type="submit">
+          <v-icon class="mr-4" size="x-large">
             mdi-email
           </v-icon>
           email my boss
         </v-btn>
       </template>
     </DetailFrame>
-    <div style="padding: 15px">
+    <div style="height: 700px; overflow: auto; padding: 15px">
       <div v-for="(mappedData, index) in mappedDataSets" :key="index">
         <div style="overflow: auto;">
           <ChartRender :chartData="mappedData" />
@@ -73,7 +43,9 @@ import DetailInput from './DetailInput.vue'
 import DetailHeader from './DetailHeader.vue'
 import DetailFrame from './DetailFrame.vue'
 import ChartRender from './ChartRender.vue'
+import emailjs from 'emailjs-com'
 import { computed, onMounted, ref } from 'vue'
+import { useState } from '../stores/state'
 import axios from 'axios'
 
 function requestHeaders() {
@@ -84,23 +56,54 @@ function requestHeaders() {
   }
 }
 
+const { user } = useState()
+
 const props = defineProps<{
   item: any
 }>()
 
+const report = computed(() => props.item)
+
 const sendEmail = async () => {
-  await axios.post(`/api/send-email`, {
-    to: 'thomas@eykamp.com',
-    subject: 'Bitch Better Have My Graphs',
-    html: `
-      <h1>
-        Give me my fucking data
-      </h1>
-    `
-  })
+  try {
+    // Create a new HTMLFormElement
+    const formElement = document.createElement('form')
+
+    // Create form controls and add them to the form element
+    const nameInput = document.createElement('input')
+    nameInput.name = 'name'
+    nameInput.value = user.name
+    formElement.appendChild(nameInput);
+
+    const reportInput = document.createElement('input')
+    reportInput.name = 'report'
+    reportInput.value = report.value.name
+    formElement.appendChild(reportInput);
+
+    const emailInput = document.createElement('input')
+    emailInput.name = 'toEmail'
+    emailInput.value = report.value.managerEmail
+    formElement.appendChild(emailInput);
+
+    const messageInput = document.createElement('input')
+    messageInput.name = 'link'
+    messageInput.value = `${window.location.href}?report=${report.value.sysId}`
+    formElement.appendChild(messageInput)
+
+    await emailjs.sendForm(
+      'service_usqd07q',
+      'template_oe59uhv',
+      formElement,
+      'V-BWoXu67Cs6ht84n'
+    )
+    console.log('success')
+
+
+  } catch (error) {
+    console.log({ error })
+  }
 }
 
-const report = computed(() => props.item)
 const dataSets = computed(() => {
   if (report.value.dataSets) {
     return report.value.dataSets.split(',').map((ds: string) => ds.trim())
