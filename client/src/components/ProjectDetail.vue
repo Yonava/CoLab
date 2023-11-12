770ccf26776a60b33359435c0bd3fca278910aa4
yonava <yona@vossandreae.com>
2023-11-11 21:42:49 -0500

    user

--- client/src/components/ProjectDetail.vue
+++ client/src/components/ProjectDetail.vue
@@ -4,6 +4,27 @@
       src="../assets/vector.svg"
       style="position: absolute; top: 10; right: -4%; width: 500px; height: 250px;"
     />
+
+    <div
+      v-if="user"
+      style="position: absolute; top: 2%; right: 1%; display: flex; justify-content: center; align-items: center; background-color: #1d68ae; border-radius: 50px; flex-direction: row; gap: 6px; padding-left: 20px; border: 1px solid black"
+    >
+      <div style="text-align: right;">
+        <div style="font-size: 1em; color: white; font-weight: bold; white-space: nowrap">
+          {{ user.name }}
+        </div>
+        <v-spacer></v-spacer>
+        <div style="font-size: 1em; color: white;">
+          ID {{ user.id.slice(0, 8) }}
+        </div>
+      </div>
+      <img
+        :src="user.picture"
+        style="width: 70px; height: 70px; border-radius: 50%; border: 2px solid white;"
+      />
+    </div>
+
+
     <div v-if="selectedReport" style="overflow: auto;">
       <ReportDetail
         :item="selectedReport"
@@ -27,5 +48,5 @@ import { storeToRefs } from 'pinia'
 import { useState } from '../stores/state'
 import ReportDetail from '../components/ReportDetail.vue'
 
-const { selectedReport } = storeToRefs(useState())
+const { selectedReport, user } = storeToRefs(useState())
 </script>
